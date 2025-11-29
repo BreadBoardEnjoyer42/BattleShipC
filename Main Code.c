80ea4857419d461e3f93745d97a408f5cccb960a
BreadBoardEnjoyer42 <bowmanw060@gmail.com>
2025-11-28 23:09:54 -0500

    Update Main Code.c

--- Main Code.c
+++ Main Code.c
@@ -2,9 +2,10 @@
 // Name: BattleShipCPE223
 // Author: Wyatt Bowman
 // Date: 11/1/25
-// Version: V0.3
-// Description: This includes a prototype of the front end code
-// Changes: Everything changed
+// Version: V0.4
+// Description: This includes a very basic placement function that allows users to place 1x1 test boats
+//              This will also serve as the algorithm for the actual placing of attacks
+// Changes: made new function, got rid of not needed comments, going to add lengths in next version
 //==========================================
 
 #include <stdio.h>
@@ -18,35 +19,21 @@ int setBoats(int, int, int, int);
 int startScreen(int);
 void showBoard(); // declaring function that shows board
 void battleShipCover_ASCII();
-void displayBoard(char player[2][100], bool, int, char playerInput[2][10][10][3], char fogPlayerInput[2][10][10][3]);
-
-int BattleArray[11][11] = {0}; // declaring array, indexing starts with one
+void displayBoard(char player[2][100], bool, int, char playerInput[2][10][10][3], char fogPlayerData[2][10][10][3]);
+void getUserBoatPlacement(char playerData[2][10][10][3], char fogPlayerData[2][10][10][3], char boardPieces[5][3], char playerName[2][100],
+                          int turn);
 
 int main()
 {
     //Start Variables
-    int userX, userY, mainScreenOption, oldMainScreenOption, rotate, turn;
+    int userX, userY, mainScreenOption, oldMainScreenOption, rotate, turn = 0;
+    int boatLength[5] = {5, 4, 3, 3, 2};
 
     char playerData[2][10][10][3]; // player, row, column, max size of char
     char fogPlayerData[2][10][10][3]; // player, row, column, max size of char
     char Ships[5][20] = {"Carrier", "Battleship", "Submarine", "Cruiser", "Destroyer"}; // names and such
     char playerName[2][100]; // make smaller in future
     char boardPieces[5][3] = {"AC","BA","CR","SU","DE"};
-
-    /*
-    UNTIL WE GET USER DATA FOR BOARD FILLING WITH STUFF
-    */
-    for (int p = 0; p < 2; p++) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                strcpy(playerData[p][i][j], "AB");
-                strcpy(fogPlayerData[p][i][j], "FG");
-            }
-        }
-    }
-    /*
-    UNTIL WE GET USER DATA FOR BOARD FILLING WITH STUFF
-    */
 
 
     bool screenShake = 0; // FOR GAME NEEDS TO BE DYNAMIC
@@ -72,60 +59,36 @@ int main()
                     int length = strlen(playerName[playerNumber]); // bad practice fix later
                     playerName[playerNumber][length - 1] = '\0'; // making sure the last real character isn't a newline
                 }
-
-            turn = 0;
-
             while(1){ // need win condition
+
+                //TEMP CODE FOR FILLING CELLS WITH EMPTY WATER
+                    if(turn == 0){
+                        for (int p = 0; p < 2; p++) {
+                            for (int i = 0; i < 10; i++) {
+                                for (int j = 0; j < 10; j++) {
+                                    strcpy(playerData[p][i][j], "  ");
+                                        strcpy(fogPlayerData[p][i][j], "  ");
+                                }
+                            }
+                        }
+                    }
+                //TEMP CODE FOR FILLING CELLS WITH EMPTY WATER
+
                 displayBoard(playerName, screenShake, turn, playerData, fogPlayerData); // just testing output
+
                 if(screenShake){
                     for(int shakeAmount = 0; shakeAmount < 4; shakeAmount++){
                         displayBoard(playerName, shakeAmount % 2 == 0, turn, playerData, fogPlayerData);
                         _sleep(500); //sleep 100ms from windows.h
                     }
                 }
+
+
+                getUserBoatPlacement(playerData, fogPlayerData, boardPieces, playerName, turn);
+
                 scanf("%d");
                 turn++;
             }
-
-/*
-        // This code is just for develpoment
-        for(int i = 0; i <= 10; i++){ // fill with zeros and creating margins
-            printf("\n\n");
-            for(int j = 0; j <= 10; j++){
-
-                if(i == 0){
-                    BattleArray[i][j] = j;
-                } else if (j == 0){
-                BattleArray[i][j] = i;
-                } else{
-                BattleArray[i][j] = 0;
-                }
-
-                printf("%d  ", BattleArray[i][j]);
-            }
-
-        }
-
-
-
-        printf("\n");
-        for(int boat = 0; boat <= 4; boat++){
-            printf("Where do you want your %s to start in the X:    ", Ships[boat]); // literally just going to assume bottom corner and horizontally for now
-            scanf("%d", &userX);
-            printf("Where do you want your %s to start in the Y:    ", Ships[boat]); // Y coord
-            scanf("%d", &userY);
-            printf("Do you want your piece Vertically: "); // one for vertical, in future take more input
-            scanf("%d", &rotate);
-            printf("\n");
-            setBoats(userX, userY, boat, rotate); // boats to control index of the actual piece
-        }
-        showBoard(); // Showing the board
-
-        while(1){} // THIS IS TEMP PLEASE DON'T LEAVE THIS WILL BREAK COMPUTER OVER TIME
-*/
-
-
-
         break;
         case 2: // HELP OPTION
             printf("\n\nVisit \"https://www.wikihow.com/Play-Battleship\" for more information\n\n");
@@ -153,24 +116,6 @@ int main()
 
 
 // START USER DEFINED FUNCTIONS
-
-// Basic Function to Set Boats down
-
-int setBoats(int xCoord, int yCoord, int boatIndex, int orientation){ //placing boats
-
-    int length[5] = {5, 4, 3, 3, 2}; // lengths of each piece
-
-    if(orientation == 1){
-        for (int iVert = 0; iVert < length[boatIndex]; iVert++) {
-            BattleArray[yCoord + iVert][xCoord] = 1; // placing boats in array with size
-        }
-    }else if(orientation == 0){
-        for (int iHor = 0; iHor < length[boatIndex]; iHor++) {
-            BattleArray[yCoord][xCoord + iHor] = 1; // placing boats in array with size
-        }
-    }
-    return 1;
-}
 
 // Function to print the selection screen
 
@@ -239,20 +184,66 @@ void displayBoard(char player[2][100], bool screenShake, int turn, char playerIn
             printf("+----");
         printf("+\n"); // last "+" before newline
     }
+
     //printf("\nScore: %d\n", score); //change for later
 }
 
+// Function for handling user input and moving around the board
 
+void getUserBoatPlacement(char playerData[2][10][10][3], char fogPlayerInput[2][10][10][3],
+                          char boardPieces[5][3], char playerName[2][100], int turn){
 
-// Function to show the data board, only needed for development
+    char input = 0, duckedCell[3] = "  ";
+    int hori, vert;
 
-void showBoard(){ // shows board, makes sense ig
-    system("cls"); // clears terminal
-for(int i = 0; i <= 10; i++){
-        printf("\n\n");
-        for(int j = 0; j <= 10; j++){
-             printf("%d  ", BattleArray[i][j]);
+    for(int i = 0; i < 5; i++){
+        hori = 0; // reset everytime NEED TO MAKE SURE IF USER PLACES BOAT AT ORIGIN WE FILTER FOR THAT
+        vert = 0;
+        strcpy(duckedCell, playerData[turn][vert][hori]); // copy the default location into the duckedCell
+        strcpy(playerData[turn][vert][hori], "[]"); //user cursor
+
+        while(input != 'q'){
+            displayBoard(playerName, 0, turn, playerData, fogPlayerInput); // show placement
+            printf("\nEnter a direction with W/A/S/D, Q when finished\n");
+            scanf(" %c", &input); // space to get rid of any accidental newline stuff
+
+            strcpy(playerData[turn][vert][hori], duckedCell); // put old cell back
+
+            if(input == 'w')
+                vert--; // make switch statement later
+            else if(input == 's')
+                vert++;
+            else if(input == 'a')
+                hori--;
+            else if(input == 'd')
+                hori++;
+            else if(input != 'q')
+                continue; //if user quits they don't have to change the state
+            else
+
+            if(vert < 0)
+                vert = 0; //checking is user input is valid or not by forcing bounds
+            if(vert > 9)
+                vert = 9;
+            if(hori < 0)
+                hori = 0;
+            if(hori > 9)
+                hori = 9;
+
+            strcpy(duckedCell, playerData[turn][vert][hori]); //store what was there before
+
+            if(strcmp(playerData[turn][vert][hori], "  ") != 0){ //checking the validity of the sqaure
+                printf("This Cell is Taken, Try Again!\n");
+                _sleep(500); // don't think I actually need this right now
+                strcpy(playerData[turn][vert][hori], duckedCell); // put old cell back
+                continue;
+            }
+
+            strcpy(playerData[turn][vert][hori], "[]"); //user cursor
         }
+    strcpy(playerData[turn][vert][hori], boardPieces[i]); // place data
+    displayBoard(playerName, 0, turn, playerData, fogPlayerInput); // display
+    input = 0; // reset the input so while loop triggers again
     }
 }
 
@@ -270,3 +261,4 @@ void battleShipCover_ASCII(){
     printf("                   \\/\\_____\\  \\ \\_\\ \\_\\  \\ \\_\\  \\ \\_\\            \n");
     printf("                    \\/_____/   \\/_/\\/_/   \\/_/   \\/_/            \n");
 }
+
