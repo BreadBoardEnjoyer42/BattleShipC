bc0f6605f2c4f04e6e5fdf3925ea0210c81d2c90
BreadBoardEnjoyer42 <bowmanw060@gmail.com>
2025-12-02 21:00:05 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 //==========================================
 // Name: BattleShipCPE223
-// Author: Kieran Cavanagh
+// Author: Wyatt Bowman
 // Date: 12/2/25
-// Version: V0.8
+// Version: V0.9
 // Description: battleships- the game
-// Changes: converting to full integer implementation.
+// Changes: Adding Attacking
 //==========================================
 
 #include <stdio.h>
 #include <string.h> // Required to use strings
 #include <stdlib.h> // Required for system()
 #include <stdbool.h>
-#include "battleshipFunctions.h"
-#include "ASCIIfunctions.h"
+#include "functions.h"
 
 
 int main(){
@@ -29,7 +28,7 @@ int main(){
 
     char ships[5][20] = {"Carrier", "Battleship", "Submarine", "Cruiser", "Destroyer"};
 
-    bool screenShake = true; // FOR GAME NEEDS TO BE DYNAMIC
+    bool screenShake = false; // FOR GAME NEEDS TO BE DYNAMIC
     //End Variables
 
     while(!(mainScreenOption == 'q' || mainScreenOption == 'Q')){ // change to when user enters new line then quits
@@ -47,8 +46,8 @@ int main(){
             value++;
                 if(value > 3)
                     value = 1;
+        system("cls"); // clears terminal
     }
-    system("cls"); // clears terminal
 
     switch(value){
 
@@ -61,9 +60,15 @@ int main(){
                 playerName[playerNumber][length - 1] = '\0'; // making sure the last real character isn't a newline
             }
 
-            for(int g = 0; g < 2; g++){
-                getUserBoatPlacement(playerData, playerName, g, boatLength, shipValueType, shipValueAbrv, ships);
-            }
+                //Player One Placement
+                getUserBoatPlacement(playerData, playerName, booleanTurn, boatLength, shipValueType, shipValueAbrv, ships);
+                //Transition
+                transistion(turn + 1);
+                //Player Two Placement
+                getUserBoatPlacement(playerData, playerName, booleanTurn + 1, boatLength, shipValueType, shipValueAbrv, ships);
+
+
+
 
             while(1){ // need win condition
 
@@ -75,11 +80,10 @@ int main(){
                         _sleep(500); //sleep 100ms from windows.h
                     }
                 }
-
-                scanf("%d");
+                attackSmack(booleanTurn, playerData, playerName, shipValueAbrv);
                 turn++;
                 booleanTurn = turn % 2;
-
+                transistion(booleanTurn);
             }
         break;
         case 2: // HELP OPTION
